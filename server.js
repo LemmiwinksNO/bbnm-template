5c21b8b6f179e5ec10f7ad9456367a25d0c8dc56
LemmiwinksNO <macreasy@gmail.com>
2013-07-14 19:23:49 -0500

    * You can now run different environments via command line. * i.e. node server.js production * Changed the way we handle dist/release - run in production and views and static are set correctly.

--- server.js
+++ server.js
@@ -17,18 +17,14 @@ var app = module.exports = express();  // export so we can use it for tests
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
+  app.set('env', process.argv[2] || 'development');  // Now you can set environment via command line.
   app.set('view engine', 'jade');
   app.set('view options', { layout: true });
-
-  app.set('views', __dirname + '/views');  // development
-  // app.set('views', __dirname + '/dist/release/views');  // production
-
+  app.set('views', __dirname + '/views');  // development views
   app.use(express.bodyParser());  // parses request body according to content type in request.
   app.use(express.methodOverride());  // Lets you make HTTP methods other than GET and POST
   app.use(app.router);
-  app.use(express.static(__dirname));
-
-  // app.use(express.static(path.join(__dirname + "/app")));
+  app.use(express.static(__dirname));  // lets you access everything rather than just /public or /app
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
 });
 
@@ -41,6 +37,8 @@ app.configure('test', function(){
 });
 
 app.configure('production', function(){
+  app.set('views', __dirname + '/dist/release/views');  // production views
+  app.use(express.static(path.join(__dirname + "/dist/release")));
   app.use(express.errorHandler());
 });
 
